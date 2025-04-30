f43e34b876c60dc69ff81d062e5b3fa14629e5c5
ggesm <117891063+ggesm@users.noreply.github.com>
2025-04-30 02:03:54 -0700

    Update lab4.js
    Fixed bugs in scripts file

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add) {
+    if (add === true) {
         let result = 0;
 
         result = num1 + num2;
